d2067181091825b288b8bd6cf3d619bc72d8beb5
andreykobal <kobalandrey@gmail.com>
2016-11-11 15:49:28 +0200

    Stable yo build

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,32 @@
-var path    = require('path')
-,   webpack = require('webpack')
-;
+'use strict';
 
-module.exports = {
-	entry: [
-		'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
-		'webpack/hot/only-dev-server',
-		'babel-polyfill',
-		'./src/js/app' // Your appʼs entry point
-    ],
-    output: {
-        filename: "dist/bundle.js"
-    },
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin(),
+const path = require('path');
+const args = require('minimist')(process.argv.slice(2));
 
-		// example of polyfilling with webpack
-		// OR just include the babel runtime option below and get Promises, Generators, Map, and much more!
-		// You can even get forward looking proposed features for ES7 and beyond with the
-		// stage query parameter below https://babeljs.io/docs/usage/experimental/ welcome
-		// to the future of JavaScript! :)
-		//
-		//new webpack.ProvidePlugin({
-		//	'arrayutils': 'imports?this=>global!exports?global.arrayutils!arrayutils'
-		//})
-	],
-	resolve: {
-		// require files in app without specifying extensions
-		extensions: ['', '.js', '.json', '.jsx', '.less'],
-		alias: {
-			// pretty useful to have a starting point in nested modules
-			'appRoot': path.join(__dirname, 'src/js'),
-			'vendor': 'appRoot/vendor'
-		}
-	},
-    module: {
-        loaders: [
-			{ test: /\.less$/,      loader: 'style-loader!css-loader!autoprefixer?browsers=last 2 version!less-loader' },
-			{ test: /\.css$/,       loader: 'style-loader!css-loader' },
-			{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}, // inline base64 URLs for <=8k images, direct URLs for the rest
-			{
-				test: /\.jsx?$/,
-				include: [
-					path.join(__dirname, 'src/js'), // files to apply this loader to
-					path.join(__dirname, 'node_modules/reflux-core')
-				],
-				// http://jamesknelson.com/using-es6-in-the-browser-with-babel-6-and-webpack/
-				loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015', 'reflux-wrap-loader'] // loaders process from right to left
-			}
-        ]
-    }
-};
+// List of allowed environments
+const allowedEnvs = ['dev', 'dist', 'test'];
+
+// Set the correct environment
+let env;
+if (args._.length > 0 && args._.indexOf('start') !== -1) {
+  env = 'test';
+} else if (args.env) {
+  env = args.env;
+} else {
+  env = 'dev';
+}
+process.env.REACT_WEBPACK_ENV = env;
+
+/**
+ * Build the webpack configuration
+ * @param  {String} wantedEnv The wanted environment
+ * @return {Object} Webpack config
+ */
+function buildConfig(wantedEnv) {
+  let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
+  let validEnv = isValid ? wantedEnv : 'dev';
+  let config = require(path.join(__dirname, 'cfg/' + validEnv));
+  return config;
+}
+
+module.exports = buildConfig(env);
